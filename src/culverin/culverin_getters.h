60923daf4313bc1ee9dd69449879a1dfb1abdb6b
Evilpasture <mandatorythebest@gmail.com>
2026-02-10 20:23:09 +0700

    move getters to a module

---
+++ src/culverin/culverin_getters.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <Python.h>
+
+// Forward declarations: "Trust me, these exist"
+struct PhysicsWorldObject;
+struct CharacterObject;
+struct VehicleObject;
+
+PyObject* get_positions(struct PhysicsWorldObject *self, void *closure);
+PyObject* get_rotations(struct PhysicsWorldObject *self, void *closure);
+PyObject *get_velocities(struct PhysicsWorldObject *self, void *c);
+PyObject *get_angular_velocities(struct PhysicsWorldObject *self, void *c);
+PyObject *get_count(struct PhysicsWorldObject *self, void *c);
+PyObject *get_time(struct PhysicsWorldObject *self, void *c);
+PyObject *get_user_data_buffer(struct PhysicsWorldObject *self, void *c);
+
+PyObject *Character_get_handle(struct CharacterObject *self, void *closure);
+
+PyObject *Vehicle_get_wheel_count(struct VehicleObject *self, void *closure);
