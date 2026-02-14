f20a6d7d0b2460a6263d9379da575bee4e07c8fa
Evilpasture <mandatorythebest@gmail.com>
2026-02-14 20:14:50 +0700

    move specific struct to header

--- src/culverin/culverin_shadow_sync.h
+++ src/culverin/culverin_shadow_sync.h
@@ -1,4 +1,9 @@
 #pragma once
 #include "culverin.h"
 
+typedef struct {
+    const JPH_Body* body;
+    uint32_t dense_idx;
+} SyncWorkItem;
+
 void culverin_sync_shadow_buffers(PhysicsWorldObject *self);
