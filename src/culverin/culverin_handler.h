8c3422cdd70fefc59530b28c8e4c1cc77bddf945
Evilpasture <mandatorythebest@gmail.com>
2026-04-29 07:49:34 +0700

    upgrade to C++

--- src/culverin/culverin_handler.h
+++ src/culverin/culverin_handler.h
@@ -50,6 +50,11 @@ int main() {
 }
 
 */
-
+#ifdef __cplusplus
+extern "C" {
+#endif
 void culv_jph_trace(const char *inString);
 bool culv_jph_assert(const char *inExpression, const char *inMessage, const char *inFile, uint32_t inLine);
+#ifdef __cplusplus
+}
+#endif
